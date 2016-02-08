3b4d7568a96e41c84490f49e9586a2fb545fdee6
Kate Studwell <katestudwell@Kates-MacBook-Pro.local>
2016-02-08 14:46:50 -0500

    Make test 8 pass. Create place and covers methods in Ship class.

--- ship.rb
+++ ship.rb
@@ -1,8 +1,24 @@
 class Ship
-  attr_reader :length
+  attr_reader :length, :x_coordinate, :y_coordinate, :ship_pegs
 
   def initialize(length)
     @length = length
   end
 
+  def place(x_coordinate, y_coordinate, across = true)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    ship_pegs = {}
+    ship_end = x_coordinate + @length - 1
+    starting_x_coordinate = x_coordinate
+    until ship_pegs.has_key?(ship_end)
+      ship_pegs[starting_x_coordinate] = y_coordinate
+      starting_x_coordinate +=1
+    end
+    @ship_pegs = ship_pegs
+  end
+
+  def covers?(x_coordinate, y_coordinate)
+    ship_pegs.has_key?(x_coordinate) && ship_pegs[x_coordinate] == y_coordinate
+  end
 end
