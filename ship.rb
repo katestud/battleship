3ac1abc910b96f9c2c7705a8eb4f6f68fffbdbbf
Kate Studwell <katestudwell@Kates-MacBook-Pro.local>
2016-02-08 15:52:07 -0500

    Make Test 9 pass. Add new hash when a ship object is placed down instead of across.

--- ship.rb
+++ ship.rb
@@ -8,17 +8,35 @@ class Ship
   def place(x_coordinate, y_coordinate, across = true)
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
+    @across = across
     ship_pegs = {}
-    ship_end = x_coordinate + @length - 1
-    starting_x_coordinate = x_coordinate
-    until ship_pegs.has_key?(ship_end)
-      ship_pegs[starting_x_coordinate] = y_coordinate
-      starting_x_coordinate +=1
+    if @across
+      ship_end = x_coordinate + @length - 1
+      starting_x_coordinate = x_coordinate
+      until ship_pegs.has_key?(ship_end)
+        ship_pegs[starting_x_coordinate] = y_coordinate
+        starting_x_coordinate +=1
+      end
+    else
+      ship_end = y_coordinate + @length - 1
+      starting_y_coordinate = y_coordinate
+      until ship_pegs.has_key?(ship_end)
+        ship_pegs[starting_y_coordinate] = x_coordinate
+        starting_y_coordinate +=1
+      end
     end
     @ship_pegs = ship_pegs
   end
 
   def covers?(x_coordinate, y_coordinate)
-    ship_pegs.has_key?(x_coordinate) && ship_pegs[x_coordinate] == y_coordinate
+    if @across
+      ship_pegs.has_key?(x_coordinate) && ship_pegs[x_coordinate] == y_coordinate
+    else
+      ship_pegs.has_key?(y_coordinate) && ship_pegs[y_coordinate] == x_coordinate
+    end
   end
 end
+
+boat = Ship.new(4)
+boat.place(2,1)
+puts boat.covers?(2,2)
