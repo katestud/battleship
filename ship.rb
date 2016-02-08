9a9db2bc19cc2b8716c0a8a3522d581a05afb03c
Kate Studwell <katestudwell@Kates-MacBook-Pro.local>
2016-02-08 17:16:26 -0500

    Make tests 8 and 9 pass again with new Position class.

--- ship.rb
+++ ship.rb
@@ -1,3 +1,5 @@
+require './position.rb'
+
 class Ship
   attr_reader :length, :x_coordinate, :y_coordinate, :ship_pegs
 
@@ -10,19 +12,21 @@ class Ship
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
     @across = across
-    ship_pegs = {}
+    ship_pegs = []
     if @across
-      ship_end = x_coordinate + @length - 1
+      ship_pegs << Position.new(@x_coordinate, @y_coordinate)
+      ship_end = x_coordinate + @length
       starting_x_coordinate = x_coordinate
-      until ship_pegs.has_key?(ship_end)
-        ship_pegs[starting_x_coordinate] = y_coordinate
-        starting_x_coordinate +=1
+      until starting_x_coordinate == ship_end
+        ship_pegs << Position.new(starting_x_coordinate, @y_coordinate)
+        starting_x_coordinate += 1
       end
     else
-      ship_end = y_coordinate + @length - 1
+      ship_pegs << Position.new(@x_coordinate, @y_coordinate)
+      ship_end = y_coordinate + @length
       starting_y_coordinate = y_coordinate
-      until ship_pegs.has_key?(ship_end)
-        ship_pegs[starting_y_coordinate] = x_coordinate
+      until starting_y_coordinate == ship_end
+        ship_pegs << Position.new(@x_coordinate, starting_y_coordinate)
         starting_y_coordinate +=1
       end
     end
@@ -31,12 +35,17 @@ class Ship
   end
 
   def covers?(x_coordinate, y_coordinate)
-    if @across
-      ship_pegs.has_key?(x_coordinate) && ship_pegs[x_coordinate] == y_coordinate
-    else
-      ship_pegs.has_key?(y_coordinate) && ship_pegs[y_coordinate] == x_coordinate
+    ship_pegs_array = []
+    ship_pegs.each do |position|
+      ship_pegs_array << ship_position_coordinates = [position.x_coordinate, position.y_coordinate]
     end
+    ship_pegs_array.include?([x_coordinate, y_coordinate])
   end
+
+
+  # def overlaps_with?(other)
+  #   ship_pegs.keys  other.covers
+  # end
 end
 
 class AlreadyBeenPlacedError < StandardError
