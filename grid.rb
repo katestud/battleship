0eea4e507606b263572b849f2424021c8fe32a3b
Kate Studwell <katestudwell@Kates-MacBook-Pro.local>
2016-02-09 13:51:20 -0500

    Make Test 17 Pass. Add display method to Grid Class.

--- grid.rb
+++ grid.rb
@@ -1,4 +1,19 @@
 class Grid
   attr_reader :ships
 
+
+
+  def display
+  puts "    1   2   3   4   5   6   7   8   9   10"
+  display_line
+  ("A".."J").each do |l|
+    puts l + " |   |   |   |   |   |   |   |   |   |   |"
+  end
+  display_line
+end
+
+private def display_line
+  puts "  -----------------------------------------"
+end
+
 end
