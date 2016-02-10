6b0bee4518ff5f963c10b90230ad9c883744ac29
Kate Studwell <katestudwell@Kates-MacBook-Pro.local>
2016-02-10 15:30:22 -0500

    Make Test 33 pass. Add call shot method to computer player class.

--- computer_player.rb
+++ computer_player.rb
@@ -1,6 +1,11 @@
 require './player'
 
 class ComputerPlayer < Player
+  def initialize
+    @y_coordinate_options = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    super()
+  end
+
   def name
     @name = "HAL 9000"
   end
@@ -9,12 +14,15 @@ class ComputerPlayer < Player
     ship_number = 0
     while (ship_number < array.length)
       ship_length = array[ship_number]
-      y_coordinate_options = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-      coordinates = "#{y_coordinate_options.sample}#{rand(1..10)}"
+      coordinates = "#{@y_coordinate_options.sample}#{rand(1..10)}"
       direction = rand(2) == 1
       ship_number +=1 if @grid.place_ship(Ship.new(ship_length), grid.x_of(coordinates), grid.y_of(coordinates), direction)
     end
     puts "HAL 9000 has placed its ships.\n"
+  end
+
+  def call_shot
+    "#{@y_coordinate_options.sample}#{rand(1..10)}"
   end
 
 
